87a0828135c1b18e3683c0c694f92de21decec74
Alexander Danmayer <dave@Daves-iMac.local>
2019-08-14 16:34:06 +0200

    remove import

--- ios/RNTLWebView-Bridging-Header.h
+++ ios/RNTLWebView-Bridging-Header.h
@@ -5,7 +5,6 @@
 //  Created by Alexander Danmayer on 12.12.18.
 //  Copyright © 2018 Faria. All rights reserved.
 //
-#import <React/RCTViewManager.h>
 #import <React/RCTEventEmitter.h>
 #import <React/RCTRootView.h>
 #import <React/RCTUIManager.h>
